a13426d558070ddcc628c6dc041fa3a214b7dfe5
ericcheung <eric.cheung@kitesystems.com>
2014-01-07 13:08:59 +0800

    init project

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        /*
+        //
+        // GET: /HelloWorld/
+        public ActionResult Index()
+        {
+            return View();
+        }
+         */
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        } 
+	}
+}
